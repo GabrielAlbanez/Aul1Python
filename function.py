5ee8d00e212ec2768edf71cc5848864aad6b689f
GabrielAlbanez <gabriel.g.albabez@gmail.com>
2024-02-28 17:21:06 -0300

    calculadora feita

--- function.py
+++ function.py
@@ -1,103 +1,64 @@
-
 import os
 
-calculosFeiots = []
+calculosFeitos = []
 
-def dataUser(x, y, operator):
+def dataUser(x, y, operador):
     resposta = None
-    if operator == "1":
+    if operador == "1":
         resposta = x + y
-    elif operator == "2":
+    elif operador == "2":
         resposta = x - y
-    elif operator == "3":
+    elif operador == "3":
         resposta = x / y
-    elif operator == "4":
+    elif operador == "4":
         resposta = x * y
     print(resposta)
     return resposta
 
 contador = 1
 
+nome = input("Digite seu nome: ")
+
+
 while True:
     os.system('cls')
-    print("conta ",contador)
-    x = float(input("digite um valor para o numero x: "))
-    y = float(input("digite um valor para o numero y: "))
-    operacao = input("digite qual operação vc quer que seja feita, 1) adição, 2 ) subtração, 3 ) divisão, 4 ) mutiplicação : ").lower()
+    print("conta", contador)
+    x = float(input(nome + ", digite um valor para o número x: "))
+    y = float(input(nome + ", digite um valor para o número y: "))
+    operacao = input(nome + ", digite qual operação você quer que seja feita (1) adição, (2) subtração, (3) divisão, (4) multiplicação): ").lower()
     resultado = dataUser(x, y, operacao)
-    calculosFeiots.append({
+    calculosFeitos.append({
         "x": x,
         "y": y,
         "operacao": operacao,
         "resposta": resultado
     })
-    pergunta = input("para continuar fazer as operações digite : continuar, caso queria para digite : pare: ")
-    contador+= 1
-    if pergunta == "pare":
+    pergunta = input(nome + ", para continuar fazendo as operações, digite 1 parar 'continuar'; caso queira parar, digite 2 para 'finalizar': ")
+    contador += 1
+    if pergunta.lower() == "2":
         os.system('cls')
         break
 
 conta = 0
-
-for calculo in calculosFeiots:
-    conta = conta + 1
+print(nome, "aqui estão todas as suas contas feitas:")
+print("----------------------------")
+for calculo in calculosFeitos:
+    conta += 1
     valorX = calculo["x"]
     valorY = calculo["y"]
     operacao = calculo["operacao"]
     nomeOperador = ""
-    if(operacao == "1"):
-      nomeOperador = "adição"
-    elif(operacao == "2"):
-      nomeOperador = "subtração"
-    elif(operacao == "3"):
-      nomeOperador = "divisão"
-    elif(operacao == "4"):
-      nomeOperador = "mutiplicação"
+    if operacao == "1":
+        nomeOperador = "adição"
+    elif operacao == "2":
+        nomeOperador = "subtração"
+    elif operacao == "3":
+        nomeOperador = "divisão"
+    elif operacao == "4":
+        nomeOperador = "multiplicação"
     
-       
-      
     valorResposta = calculo["resposta"]
-    print("conta:", conta)
-    print("x:", valorX, "y:", valorY, "operacao:", nomeOperador, ", resposta:", valorResposta)
+    
+    print("Conta:", conta)
+    print("x:", valorX, "y:", valorY, "operação:", nomeOperador, ", resposta:", valorResposta)
     print("--------------------------------")
-
-  
-
-
-    
-    
-     
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# alunos = []
-
-
-# for i in range(3):
-#   os.system('cls')
-#   nome = input("digite seu nome: ")
-#   idade = input("digite sua idade:")
-#   resposta  = [nome,idade]
-#   alunos.append(resposta)
-  
-  
-  
-# for al in alunos:
-#   nomeAluno = al[0]
-#   idadeAluno = al[1]
-#   print("Aluno: ",nomeAluno,"idadade: ",idadeAluno)
-
